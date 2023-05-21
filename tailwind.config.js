a7c1b434a4d83e33f49c6e36416a0365e883339b
allentzlu <77366058+allentzlu@users.noreply.github.com>
2023-05-21 03:52:53 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,20 @@ module.exports = {
     extend: {
       colors: {
         white: "#fff",
-        mediumseagreen: "#79c677",
         gray: {
           "100": "#97866c",
           "200": "#111e30",
           "300": "#020b2c",
         },
-        black: "#000",
-        "dark-grey": "#616161",
-        "light-grey": "#d5d5d5",
         silver: "#c4c4c4",
         gainsboro: {
           "100": "#d9d9d9",
           "200": "#d8d8d8",
         },
+        black: "#000",
+        "dark-grey": "#616161",
+        "light-grey": "#d5d5d5",
+        mediumseagreen: "#79c677",
         red: "#e60000",
         seashell: "#fffaf5",
         lightslategray: "#6c8f97",
@@ -32,15 +32,15 @@ module.exports = {
         "roboto-mono": "'Roboto Mono'",
       },
       borderRadius: {
-        "8xs": "5px",
         "6xs": "7px",
+        "8xs": "5px",
         "10xs-1": "2.1px",
       },
     },
     fontSize: {
+      base: "16px",
       xl: "20px",
       "31xl": "50px",
-      base: "16px",
       xs: "12px",
       sm: "14px",
       "sm-4": "13.4px",
